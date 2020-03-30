e36511ea251c05cdf4657b16eafbf330ebae1c8e
Jonathas Honorato <jonathas@imacs-imac.lan>
2020-03-29 21:27:05 -0400

    End session 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,11 +27,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt=>{
+            services.AddDbContext<DataContext>(opt =>
+            {
 
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+
+            });
 
             services.AddControllers();
         }
@@ -50,10 +60,19 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapControllers();
             });
+
+
+
+
+
+
         }
     }
 }
